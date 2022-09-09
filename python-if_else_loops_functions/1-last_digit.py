f297909d6d88e9ef31a9caa300126b5050536bcc
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-10 00:24:02 +0200

    Update 1-last_digit.py
    using strings in each case to try to print all cases

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,12 +6,10 @@ if number > 0:
 else:
     last_digit = number % 10
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is grater than 5")
+    str_to_print = "and is grater than 5"
 elif number <= 5:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-
-
-
+    str_to_print = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {last_digit} str")
 
 
 #number_string = repr(number)
