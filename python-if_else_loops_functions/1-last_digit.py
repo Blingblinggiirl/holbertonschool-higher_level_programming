ece8f050d5b39ede817b1bf71d1a16856cb1c353
Blingblinggiirl <5075@holbertonschool.com>
2022-09-09 12:30:37 -0700

    reconoce que el ultimo digito pude ser menor que 6 y no 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,6 @@ if last_digit_int > 5:
     print(f"Last digit of {number} is {last_digit_int} and is grater than 5")
 if last_digit_int == 0:
     print(f"Last digit of {number} is {last_digit_int} and is 0")
-if last_digit_int < 6:
-    print(f"Last digit of {number} is {last_digit_int} and is less than 6")
+if last_digit_int < 6 and last_digit_int != 0:
+    print(f"Last digit of {number} is {last_digit_int} and is less than 6 and not 0")
+
