be93d56ebc2652fe24540689ce9ce870b088bc32
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-09 23:26:07 +0200

    Update 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number_string = repr(number)
 last_digit_string = number_string[-1]
 last_digit_int = int(last_digit_string)
 if last_digit_int < 0:
-    last_digit_int = int(last_digit_int % 10) * -1
+    last_digit_int = int((last_digit_int % 10) * -1)
 if last_digit_int > 5:
     print(f"Last digit of {number} is {last_digit_int} and is grater than 5")
 if last_digit_int == 0:
