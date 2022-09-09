81103b2225b34e650bd572c812391a1921ae10ff
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-10 00:10:24 +0200

    Update 1-last_digit.py
    intentando setear elvalor de last-digit segun el modulo del numero

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 == 0:
-    last_digit = number % 10
-    print(f"Last digit of {number} is {last_digit} and is 0")
-if number % 10 != 0:
+if number % 10 > 0:
     last_digit = -(-number % 10)
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-if number % 10 > 5:
+if number % 10 >= 0:
     last_digit = number % 10
+if last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is 0")
+if last_digit % 10 > 5:
     print(f"Last digit of {number} is {last_digit} and is grater than 5")
 elif number % 10 <= 5:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
