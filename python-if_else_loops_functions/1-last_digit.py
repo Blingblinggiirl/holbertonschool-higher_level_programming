42f8ecbb04c499be80741d1918c6eee94a1de21d
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-09 23:22:26 +0200

    Update 1-last_digit.py
    trying to get a - after the last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,6 +4,8 @@ number = random.randint(-10000, 10000)
 number_string = repr(number)
 last_digit_string = number_string[-1]
 last_digit_int = int(last_digit_string)
+if last_digit_int < 0:
+    last_digit_int = int(last_digit_int % 10) * -1
 if last_digit_int > 5:
     print(f"Last digit of {number} is {last_digit_int} and is grater than 5")
 if last_digit_int == 0:
