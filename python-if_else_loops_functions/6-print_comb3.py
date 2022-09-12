788d8838840e8e5117ff2c7078ddd0cc9ef01eb9
Blingblinggiirl <5075@holbertonschool.com>
2022-09-11 17:58:38 -0700

    fixing bad identation

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,9 +3,9 @@ for i in range(0, 100):
     if (i % 10 != i / 10): #if last digit is diferent to the first
         if (i % 10 <= i / 10): #if last digit is menor to the first
             continue
-        if (i % 10 == i / 10):
-            continue
-        else:
-            print("{:02d}".format(i), end=', ')
-        if (i == 89):
-            print("{:02d}".format (i))
+    if (i % 10 == i / 10):
+        continue
+    else:
+        print("{:02d}".format(i), end=', ')
+    if (i == 89):
+        print("{:02d}".format (i))
