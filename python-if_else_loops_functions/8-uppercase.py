ba9409f8862491a01bae4b66eb5ae25dc40c639c
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-12 04:19:02 +0200

    Update 8-uppercase.py
    identation and pep8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str: 
+    for i in str:
         letra = ord(i)
         if letra > 96 and letra < 123:
             letra = letra - 32
-        print("{}".format(chr(letra)), end='')
-    print('')
+            print("{}".format(chr(letra)), end='')
+        print('')
