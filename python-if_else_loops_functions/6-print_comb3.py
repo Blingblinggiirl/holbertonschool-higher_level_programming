c84fb4c7862c82b296fdc6077fb2d82ccc6c4b05
Blingblinggiirl <5075@holbertonschool.com>
2022-09-11 18:05:27 -0700

    triying put some things in diferent order

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if (i % 10 != i / 10): #if last digit is diferent to the first
-        if (i % 10 <= i / 10): #if last digit is menor to the first
+    if (i / 10 != i % 10):
+        if (i % 10 <= i / 10):
             continue
-    if (i % 10 == i / 10):
+        if (i == 89):
+            print("{:02d}".format(i))
+        else:
+            print("{:02d}".format(i), end=', ')
+    else: 
         continue
-    else:
-        print("{:02d}".format(i), end=', ')
-    if (i == 89):
-        print("{:02d}".format (i))
