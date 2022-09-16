2f193e0e73c66de1b9c24a6ca64f369deec7fe83
Blingblinggiirl <5075@holbertonschool.com>
2022-09-16 14:50:59 -0700

    coso borde idx < 0 y reemplazando la posicion de i en my list por el element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
     for i in range(0, len(my_list)):
         if i == idx:
-            return[element]
-        else:
-            return[i]
+            my_list[i] = element
+    return element
