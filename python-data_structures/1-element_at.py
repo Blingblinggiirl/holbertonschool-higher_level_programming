bd8de59353650c7fc88140a0bc2a3a1f102c51be
Blingblinggiirl <5075@holbertonschool.com>
2022-09-16 14:48:44 -0700

    deleting some commits made by mistake

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):if idx < 0:
-        if idx < 0:
-            return my_list
-        for i in range(0, len(my_list)):
-            if i == idx:
-                my_list[i] = element
-        return element
+def element_at(my_list, idx):
+    for i in range(0, len(my_list)):
+        if i == idx:
+            return my_list[i]
