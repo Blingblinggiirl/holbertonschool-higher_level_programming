1d7a6fe0d912030ec0b967da28393f7792155d49
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-16 23:34:07 +0200

    Update 1-element_at.py
    change in th return

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element_at(my_list, idx):if idx < 0:
             return my_list
         for i in range(0, len(my_list)):
             if i == idx:
-                return my_list[i]
+                return element
