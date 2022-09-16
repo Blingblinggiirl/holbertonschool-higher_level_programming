6d0ab06013069b623ad7836b027731dfc3c7fded
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-16 23:32:22 +0200

    Update 1-element_at.py
    adding border case

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    for i in range(0, len(my_list)):
-        if i == idx:
-            return my_list[i]
+def element_at(my_list, idx):if idx < 0:
+        if idx < 0:
+            return my_list
+        for i in range(0, len(my_list)):
+            if i == idx:
+                return my_list[i]
