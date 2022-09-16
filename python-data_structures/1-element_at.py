0375263772b286bdcd8a1be5c133a0aa792be638
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-16 23:36:09 +0200

    Update 1-element_at.py
    bad identation, also iguale the i position of my list to element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,4 +4,5 @@ def element_at(my_list, idx):if idx < 0:
             return my_list
         for i in range(0, len(my_list)):
             if i == idx:
-                return element
+                my_list[i] = element
+        return element
