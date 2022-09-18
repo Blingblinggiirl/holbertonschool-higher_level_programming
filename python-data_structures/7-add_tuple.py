1b302d9b1989930d5978d450ae930e38f8f8f85a
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-19 00:11:27 +0200

    Update 7-add_tuple.py
    using incorrectly for. fixed with if.
    addin a return

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-  for len(tuple_a) < 2 :
+  if len(tuple_a) < 2 :
     new_a = tuple_a + (0, ) + (0, )
-  for len(tuple_b) < 2:
+  if len(tuple_b) < 2:
     new_b = tuple_b + (0, ) + (0, )
   both = (new_a[0] + new_[0], new_a[1] + new_b[1])
+  return both
