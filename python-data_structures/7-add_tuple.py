f820b062414828f0a9f4ba98fd41218cc815a741
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-19 00:13:28 +0200

    Update 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,5 +4,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     new_a = tuple_a + (0, ) + (0, )
   if len(tuple_b) < 2:
     new_b = tuple_b + (0, ) + (0, )
-  both = (new_a[0] + new_[0], new_a[1] + new_b[1])
+  both = (new_a[0] + new_b[0], new_a[1] + new_b[1])
   return both
