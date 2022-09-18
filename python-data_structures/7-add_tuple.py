e5c309ca2b0166c7340fbdccdbb4f0291f8799a1
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-19 00:15:28 +0200

    Update 7-add_tuple.py
    change in the name of the variables

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
   if len(tuple_a) < 2 :
-    new_a = tuple_a + (0, ) + (0, )
+    tuple_a = tuple_a + (0, ) + (0, )
   if len(tuple_b) < 2:
-    new_b = tuple_b + (0, ) + (0, )
-  both = (new_a[0] + new_b[0], new_a[1] + new_b[1])
+    tuple_b = tuple_b + (0, ) + (0, )
+  both = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
   return both
