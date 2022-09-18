fb9ef921b9b86f2024e6e3240c9e93fac4cdb45c
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-19 00:05:11 +0200

    Update 8-multiple_returns.py
    create a variable for the len of sentence, add a return in the last line

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-  if len(sentence) != 0:
-    return len(sentence), sentnce[0]
+  lens = len(sentence)
+  if lens != 0:
+    return lens, sentnce[0]
   else:
-    None
+    return None
