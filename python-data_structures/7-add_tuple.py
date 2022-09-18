5d50d46cfa81db756a5b6a9fcbed01973f32825f
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-18 23:50:48 +0200

    Update 7-add_tuple.py
    adding b, change the if for a for

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-  if len(tuple_a) < 2 :
+  for len(tuple_a) < 2 :
     new_a = tuple_a + (0, ) + (0, )
+  for len(tuple_b) < 2:
+    new_b = tuple_b + (0, ) + (0, )
+  both = (new_a[0] + new_[0], new_a[1] + new_b[1])
