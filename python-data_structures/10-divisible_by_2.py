56b189144eb6fc5ae72777dcaa5bfffcad4bb6e9
Blingblinggiirl <5075@holbertonschool.com>
2022-09-18 18:42:06 -0700

    pep8 validation

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-  mol = []
-  for i in my_list:
-    if i % 2 == 0:
-      mol.append(True)
-    else:
-      mol.append(False)
-  return mol
-    
+    mol = []for i in my_list:
+        if i % 2 == 0:
+            mol.append(True)
+        else:
+            mol.append(False)
+            return mol
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-  my_list.sort()
-  if my_list == None:
-    return None
-  elif len(my_list) > 0:
-    return my_list[-1]
+    my_list.sort()
+    if my_list == None:
+        return None
+    elif len(my_list) > 0:
+        return my_list[-1]
