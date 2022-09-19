27eb7a372b23ccf344ce69716118603f93358bb2
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-19 03:45:20 +0200

    pep8

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    mol = []for i in my_list:
+    mol = []
+    for i in my_list:
         if i % 2 == 0:
             mol.append(True)
         else:
