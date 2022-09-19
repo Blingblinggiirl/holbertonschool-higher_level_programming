13dce9947126a1796b29d73c56b1210dc875cd85
Blingblinggiirl <5075@holbertonschool.com>
2022-09-18 18:31:23 -0700

    fix

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     mol = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            mol.append(replace)
-        else:
-            mol.append(my_list[i])
-        return mol
+    if my_list != None:
+        mol = list(map(lambda e: replace if e == search else e, my_list))
+    else:
+        return None
+    return mol
