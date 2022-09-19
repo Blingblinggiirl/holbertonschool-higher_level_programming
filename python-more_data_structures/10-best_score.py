990d45a802da2c78325d2a49e08e3883d0223279
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-19 04:23:45 +0200

    fixing if

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(dic):
     score = 0
     posible_key = None
-    if dic != None:
+    if dic is not None:
         for key, value in dic.items():
             if value > score:
                 posible_key = key
