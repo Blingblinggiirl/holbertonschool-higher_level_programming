f3c74bbeda74eee66ed80a6878c0df5e7c1fe4dd
Blingblinggiirl <5075@holbertonschool.com>
2022-09-21 11:06:26 -0700

    size

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" Square with size, area, only with ints """
+""" square with size, size must be an integer otherwise, error message """
 
 
 class Square:
-    """ size and area """
-    def __init__(self, size=0):  
-        if not isinstance(size, int)  # to know if is an int:
+    """ size things """
+    def __init__(self, size=0):
+        if not isinstance(size, int):  # to indicate if is not an int
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    """ area """
-    def area(self):        
