35e89cd331df67d8971973df4065f432fc6ba973
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-21 18:25:32 +0200

    fixing comments errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" square with size, size must be an integer otherwise show an error message """
+""" square with size, size must be an integer otherwise, error message """
 
 
 class Square:
     """ size things """
     def __init__(self, size=0):
-        if not isinstance(size, int): #to indicate if is not an int
+        if not isinstance(size, int):  # to indicate if is not an int
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
