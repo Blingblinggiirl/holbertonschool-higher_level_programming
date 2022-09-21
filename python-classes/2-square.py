e043bc45fe03d512b25b699b791cbcdb902c849d
Blingblinggiirl <5075@holbertonschool.com>
2022-09-21 09:09:25 -0700

    identat0c24e3f9ae0abb116c4dion fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,11 +3,9 @@
 
 
 class Square:
-  """ size things """
-  def __init__(self, size=0):
-    if not isinstance(size, int): #to indicate if is not an int
-      TypeError("size must be an integer")
-    if size > 1:
-      ValueError("size must be >= 0")
+    def __init__(self, size=0):
+        if not isinstance(size, int): #to indicate if is not an int
+            TypeError("size must be an integer")
+        if size > 1:
+            ValueError("size must be >= 0")
     self.__size = size
-    
