fd527fec8d6a5702b8e27521deaf42e6b3f3112d
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-21 18:20:57 +0200

    size can be 0 too

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,6 +7,6 @@ class Square:
   def __init__(self, size=0):
     if not isinstance(size, int): #to indicate if is not an int
       raise TypeError("size must be an integer")
-    if size < 1:
+    if size < 0:
       raise ValueError("size must be >= 0")
     self.__size = size
