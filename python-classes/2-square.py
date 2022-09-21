487353bda2c641f6331d5aecbda9c31d75b26a36
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-21 18:19:18 +0200

    if condition for size < 1 was wrong
    Also, TypeError and ValuError estaban mal utilizados

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
   """ size things """
   def __init__(self, size=0):
     if not isinstance(size, int): #to indicate if is not an int
-      TypeError("size must be an integer")
-    if size > 1:
-      ValueError("size must be >= 0")
+      raise TypeError("size must be an integer")
+    if size < 1:
+      raise ValueError("size must be >= 0")
     self.__size = size
