733b3f51112add107d4e086ca731db2d5c1e8925
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-21 18:03:47 +0200

    try task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" square with size, size must be an integer otherwise show an error message """
+
+
+class Square:
+  """ size things """
+  def __init__(self, size=0):
+    if not isinstance(size, int): #to indicate if is not an int
+      TypeError("size must be an integer")
+    if size > 1:
+      ValueError("size must be >= 0")
+    self.__size = size
+    
