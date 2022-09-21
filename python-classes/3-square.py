90bf94a85b756f6dc8dd5db25fae13afe25fc441
Blingblinggiirl <5075@holbertonschool.com>
2022-09-21 11:02:07 -0700

    comment module

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,5 +10,5 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-     def area(self):
-        
+    """ area """
+    def area(self):        
