ac6904dc3bfa7ad3d4b7236df060cafe677a8c4a
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-23 00:46:02 +0200

    adding another for

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -27,7 +27,8 @@ class Square:
     def my_print(self):
         """ time for print """
         for i in range(self.__size):
-            print("#", end="")
+            for i in range(self.__size)
+                print("#", end="")
             print()
         if self.__size == 0:
             print()
