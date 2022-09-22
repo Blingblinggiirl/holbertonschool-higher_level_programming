a9dcb2f463da137462d01c18a4ff1698d3843694
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-22 22:35:16 +0200

    gettin size initialized

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,3 +4,15 @@ class Square:
     """ class Square """
     def __init__(self, size=0):
         self.__size = size
+        
+        
+    def size(self):
+        """ size """
+        return self.__size
+    
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("must be an integer")
+        
+    
+    
