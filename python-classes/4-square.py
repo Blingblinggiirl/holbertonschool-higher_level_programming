dfe9f616316e203a00a6d0d088aa9eb6eb12794a
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-22 23:12:58 +0200

    pep 8

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,12 +6,12 @@ class Square:
     """ class Square """
     def __init__(self, size=0):
         self.__size = size
-        
+
     @property
     def size(self):
         """ size """
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -19,7 +19,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be > 0")
         self.__size = value
-        
+
     def area(self):
         """ area """
         return (self.__size * self.__size)
