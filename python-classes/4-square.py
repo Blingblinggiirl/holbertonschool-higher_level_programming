16d772327d8545174bb57d97e3720d21ef7e8a5a
Blingblinggiirl <5075@holbertonschool.com>
2022-09-22 13:55:57 -0700

    fixing commenting

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ class squarethat defines a square """
+
+
 class Square:
     """ class Square """
     def __init__(self, size=0):
         self.__size = size
         
-    @property    
+    @property 
     def size(self):
         """ size """
         return self.__size
@@ -17,7 +19,8 @@ class Square:
         if value < 0:
             raise ValueError("size must be > 0")
         self.__size = value
-     def area(self):
+
+    def area(self):
         """ area """
         return (self.__size * self.__size)
     
