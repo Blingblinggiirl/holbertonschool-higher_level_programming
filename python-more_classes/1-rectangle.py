16f77b7dbea5581e25cec1592100fde8596b1071
Blingblinggiirl <5075@holbertonschool.com>
2022-09-24 14:38:01 -0700

    typeError intead of value

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     def width(self, value):
         """to set"""
         if not isinstance(value, int):
-            raise ValueError("width must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -32,7 +32,7 @@ class Rectangle:
     def height(self, value):
         """to set altura"""
         if not isinstance(value, int):
-            raise ValueError("height must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
