58c6452db48f598c5da81e44c78c9f6a5d599fcf
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-24 16:28:20 +0200

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,14 +6,15 @@ def add_integer(a, b=98):
     """ is not int or float, error """
     if (int(a), int(b)):
         return a + b
-    elif (a is not isin0stance(a, int) and (not isinstance(a, float))):
+    elif (a is not isinstance(a, int) and (not isinstance(a, float))):
         raise TypeError("a must be an integer")
     elif (b is not isinstance(b, int) and (not isinstance(b, float))):
         raise TypeError("b must be an integer")
     elif (a == 0) or (b == 0):
         raise TypeError("a and/or b missing")
-    if type(a) != int:
+    elif type(a) != int:
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    elif type(b) != int:
         raise TypeError("b must be an integer")
+    if 
     return int(a) + int(b)
