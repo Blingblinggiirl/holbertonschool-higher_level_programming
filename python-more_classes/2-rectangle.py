bf878dd26deb73ece2c6c19f41c93f038be366d9
Blingblinggiirl <5075@holbertonschool.com>
2022-09-24 15:00:02 -0700

    pep8 validation

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -35,11 +35,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def area(self):
         """area of the reactangle"""
         return (self.__width * self.__height)
-    
+
     def perimeter(self):
         """perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
