1633ce38c80400060b0bbb13ae97a1c5182d93cd
Blingblinggiirl <5075@holbertonschool.com>
2022-09-24 14:41:18 -0700

    pep 8 validation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     def width(self, value):
         """to set"""
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+           raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -36,4 +36,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
