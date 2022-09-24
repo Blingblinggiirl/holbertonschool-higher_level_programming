8cbf07218aed268cefaf920d1df34883977b1aa4
Blingblinggiirl <5075@holbertonschool.com>
2022-09-24 14:29:38 -0700

    height retrieve and set

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,3 +23,17 @@ class Rectangle:
             raise TypeError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """to set altura"""
+        if isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value > 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
+
