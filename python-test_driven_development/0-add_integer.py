fbbeb7cb24b802950929e2daaa57531f1c7bc0fe
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-24 16:37:59 +0200

    setting floats into ints

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,14 +6,14 @@ def add_integer(a, b=98):
     """ is not int or float, error """
     if (int(a), int(b)):
         return a + b
-    elif (a is not isin0stance(a, int) and (not isinstance(a, float))):
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    elif (a is not isinstance(a, int) or (not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    elif (b is not isinstance(b, int) and (not isinstance(b, float))):
+    elif (b is not isinstance(b, int) or (not isinstance(b, float))):
         raise TypeError("b must be an integer")
     elif (a == 0) or (b == 0):
         raise TypeError("a and/or b missing")
-    if type(a) != int:
-        raise TypeError("a must be an integer")
-    if type(b) != int:
-        raise TypeError("b must be an integer")
     return int(a) + int(b)
