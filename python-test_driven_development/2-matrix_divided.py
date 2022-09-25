b7e4ae4bedab524f8fd640d27020efe02837d7d6
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-26 01:04:03 +0200

    pep 8 validation, module documented, bad sintaxys

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""
-        matrix
-"""
+""" matrix """
 
 
 def matrix_divided(matrix, div):
-    """matrix dividir"""
+    """ matrix dividir """
     matrix_len = len(matrix[0])
     for row in matrix:
-        if matrix_len =! len(row):
+        if matrix_len != len(row):
             raise TypeError("Each row of the matrix must have the same size")
         for x in row:
             if not isinstance(x, int):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             return None
        if not isinstance(matrix, list) or not matrix[0]:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if (not isinstance(div, int) and (not isinstance(div, float))):
         raise TypeError("div must be a number")
     if div == 0:
