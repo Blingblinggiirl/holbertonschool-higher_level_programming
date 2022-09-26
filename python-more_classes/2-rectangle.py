f52f1d36f7221c17a1cee45f71769c14c01a8323
Romina Benitez <105665505+Blingblinggiirl@users.noreply.github.com>
2022-09-26 04:39:34 +0200

    Update 2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """ rectangle """
-    def __init__(self, width, height=0):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
