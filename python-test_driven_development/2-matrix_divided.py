3ef1ae48dc1674f3acf58add3a47b33032af2480
Blingblinggiirl <5075@holbertonschool.com>
2022-09-25 17:32:15 -0700

    upload

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""" matrix divided """
+"""matrix_divided"""
 
 
 def matrix_divided(matrix, div):
-    """ Matrix dividir """
-    matrix_len = len(matrix[0])
-    for row in matrix:
-        if matrix_len != len(row):
+    """divided a matrix"""
+    length_matrix = len(matrix[0])
+    or row in matrix:
+        if length_matrix != len(row):
             raise TypeError("Each row of the matrix must have the same size")
+
         for x in row:
             if not isinstance(x, int):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            return None
 
-        if not isinstance(matrix, list) or not matrix[0]:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
+                return None
+    if not isinstance(matrix, list) or not matrix[0]:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if (not isinstance(div, int) and (not isinstance(div, float))):
         raise TypeError("div must be a number")
     if div == 0:
-        raise ZeroDivisionError("division by zero") 
-    matriz_dividida= matrix.copy()
+        raise ZeroDivisionError("division by zero")
+    matriz_dividida = matrix.copy()
     matriz_dividida = [list(map(lambda x: round(x / div, 2), roow)) for roow in matrix]
     return matriz_dividida
