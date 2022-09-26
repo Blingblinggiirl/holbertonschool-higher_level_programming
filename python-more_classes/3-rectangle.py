276e584f65ae1ddae1130ed2e5ac50d6ee9a1a24
Blingblinggiirl <5075@holbertonschool.com>
2022-09-25 18:25:45 -0700

    fixing trouble

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-""" class rectangle that define a rectangle """
+"""a class Rectangle that defines a rectangle"""
 
 
 class Rectangle:
-    """ rectangle """
-    def __init__(self, width, height=0):
-        self.__width = width
-        self.__height = height
+    """Class of the rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """retrieve width"""
+        """retirbe the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """to set"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -24,32 +23,33 @@ class Rectangle:
 
     @property
     def height(self):
-        """retrieve height"""
+        """retrives the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """to set altura"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """area of the reactangle"""
-        return (self.__width * self.__height)
+        """returns the rectangle area"""
+        return self.width * self.height
 
     def perimeter(self):
-        """perimeter of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
+        """returns the rectangle perimeter"""
+        if self.width == 0 or self.height == 0:
             return 0
-        else:
-            return self.__width + self.__width + self.__height + self.__height
-    def print(self):
-        """time to print"""
-        for i in range(self.__width):
-            for j in range(self.__height):
-                print("#", end="")
-        if self.__width == 0 or self.__height == 0:
-            print()
+        return (2 * (self.height + self.width))
+
+    def __str__(self):
+        """retrive a new string to it"""
+        space_string = ""
+        if self.width != 0:
+            for i in range(self.height):
+                space_string += "#" * self.width
+                if i != self.height - 1:
+                    space_string += "\n"
+        return space_string
