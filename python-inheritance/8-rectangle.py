31a046fc8210c3d32fc409169920975b30aa4df1
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-28 10:12:46 -0700

    documentclass

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" task 8 """
+""" Task 8 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -7,7 +7,7 @@ class Rectangle(BaseGeometry):
     """ Define a new task based on BaseGeometry """
 
     def __init__(self, width, height):
-        """new class defined based on BaseGeometry"""
+        """New class defined based on BaseGeometry"""
 	self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
