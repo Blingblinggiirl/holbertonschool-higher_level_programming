3308e243bff7cd8c08e97daa26ae68606eb9a1e9
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-28 15:10:12 -0700

    bad return

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,4 +7,4 @@
 def write_file(filename="", text=""):
     """ UTF8 and numb of chars """
     with open(filename, 'w', encoding='UTF-8') as file:
-        return file,write(text)
+        return file.write(text)
