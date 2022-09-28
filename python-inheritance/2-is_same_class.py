b78c4ee5cb2a3043ec091ee0d40154c6948a3b70
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-27 19:03:19 -0700

    pep 8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Return True if obj is exactly an instance of a specified class, false otherwise"""
+"""Return True if obj is exactly an instance
+of a specified class, false otherwise"""
 
 
 def is_same_class(obj, a_class):
