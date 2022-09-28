f10e13757223f6f2c4336d0df058f0521efb061c
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-28 15:07:28 -0700

    try 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""     task 1: write a string to text file and 
+        return num of chars
+"""
+
+
+def write_file(filename="", text=""):
+    """ UTF8 and numb of chars """
+    with open(filename, 'w', encoding='UTF-8') as file:
+        return file,write(text)
