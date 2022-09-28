75114cce3d7575a7472e30a3a311640b11c988b2
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-27 19:01:10 -0700

    return if its True if the object is exactly an instance of the specified class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Return True if obj is exactly an instance of a specified class, false otherwise"""
+
+
+def is_same_class(obj, a_class):
+    """rwturs Trye or false depending if is instance"""
+    return subclass(a_class, type(obj))
