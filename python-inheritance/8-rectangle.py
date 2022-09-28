dd26456a797d4e3449e8a40c8da74a5d1a95d11a
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-28 10:16:26 -0700

    documentclass

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Rectancle class """
+"""Rectangle class"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Define a new task based on BaseGeometry """
+    """New class based on BaseGeometry class"""
 
     def __init__(self, width, height):
-        """New class defined based on BaseGeometry"""
-	self.integer_validator("width", width)
+        """To create a new instance"""
+        self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
