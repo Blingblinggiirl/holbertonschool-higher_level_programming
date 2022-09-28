0c4d6b01b5332a3986ed487e14f45e64367bc850
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-28 10:02:44 -0700

    first try

---
+++ python-inheritance/8-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""task 8"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Define a new task based on BaseGeometry"""
+
+    def __init__(self, width, height):
+        """ Only creating new instances verified by integer_validator """
+	self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
