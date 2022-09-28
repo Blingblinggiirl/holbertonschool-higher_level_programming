0605b8c4484532a42d29cd6f5c81780e20ba4209
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-28 10:48:37 -0700

    pep8

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,6 +16,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """Class that define a new class based on BaseGeometry class"""
 
