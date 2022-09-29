e8ffd094a4d7a8e0e835e2aedaf683cfd29ebb8d
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-28 18:08:04 -0700

    maybe 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" task 10 """
+
+
+class Student:
+    """ student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        listt = {}
+        self_dict = self.__dict__
+
+        if attrs is None:
+            return self_dict
+        elif type(attrs) is list:
+            for i in attrs:
+                if hasattr(self, i):
+                    listt[i] = getattr(self, i)
+            return listt
+
