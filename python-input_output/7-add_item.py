82842e8c5466c10e2270935e228f8cb68b23004c
Blingblinggiirl <romibenitezz87@gmail.com>
2022-09-28 17:29:21 -0700

    trying plan b

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""     task 7      """
+""" 	task 7 		"""
 
-
-from sys import argv
+import json, sys, os.path
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+args = sys.argv[1:]
+new_list = []
 
-try:
-    load = load_from_json_file(filename)
-except FileNotFoundError:
-    load = []
+if os.path.exists(filename):
+    new_list = load_from_json_file(filename)
 
-save_to_json_file(load + argv[1:], filename)
+for item in args:
+    new_list.append(item)
+save_to_json_file(new_list, filename)
