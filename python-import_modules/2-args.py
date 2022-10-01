11bfd116b99752555bc609fb44eecc34249e68ce
Blingblinggiirl <romibenitezz87@gmail.com>
2022-10-01 10:56:27 -0700

    do

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    import syis
     if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) == 2:
         print("1 argument:")
-        print("1: {}".format(len(sys.argv) - 1))
+        print("1:{}".format(len(sys.argv))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(len(sys.argv)):
