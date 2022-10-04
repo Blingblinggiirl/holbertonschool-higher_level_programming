1168903b51a63372c98f98fb74f4fd1f9f0f904a
Blingblinggiirl <romibenitezz87@gmail.com>
2022-10-04 14:19:26 -0700

    initial commit

---
+++ python-almost_a_circle/README.md
@@ -0,0 +1,2 @@
+readme for the almost a circle project
+task 1: create a base class of all other classes in the project. It works for manage id attribute in the next tasks
---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/9usr/bi/python3
+""" Creating an class called base """
+
+
+class Base:
+    """Class base:
+    private class attribute __nb_objects
+    class constructor: init(self, id=None)
+        """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initializing Base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nd_objects
