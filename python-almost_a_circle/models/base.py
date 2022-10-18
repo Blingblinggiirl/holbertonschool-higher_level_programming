074fb6cc224dab39256df5d65617487c43e90f2f
Blingblinggiirl <romibenitezz87@gmail.com>
2022-10-18 16:02:03 -0700

    base.py task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-""" Creating an class called base """
-
+"""
+    Class Base 
+"""
 
 class Base:
-    """Class base:
-    private class attribute __nb_objects
-    class constructor: init(self, id=None)
-        """
+    """ Class """
     __nb_objects = 0
+    
     def __init__(self, id=None):
-        """Initializing Base class"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nd_objects
+            self.id = Base.__nb_objects
