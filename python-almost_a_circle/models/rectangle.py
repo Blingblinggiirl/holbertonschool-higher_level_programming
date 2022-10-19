61b8442fccec4920dcb4563c53a9c69c7c22c4c0
Blingblinggiirl <romibenitezz87@gmail.com>
2022-10-19 07:58:03 -0700

    class rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!iusr/bin/python3
+"""
+    class rectangle that inherits from Base
+"""
+
+from models.base import Base
+
+
+class rectangle(Base):
+    """ Rectangle """
+    @property 
+    def width(self):
+        self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = width
+
+    @property
+    def height(self):
+        self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = height
+
+    @property
+    def x(self):
+        self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = x
+
+    @property
+    def y(self):
+        self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = y
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ init """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
