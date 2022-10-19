0f3157feffc04e8b944e6774a9055c317411a55c
Blingblinggiirl <romibenitezz87@gmail.com>
2022-10-19 16:06:10 -0700

    update task 3 rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,38 +9,49 @@ from models.base import Base
 class Rectangle(Base):
     """ Rectangle """
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ init """
+    def __init__(self,width, height, x=0, y=0, id=None):
+        """init with super init so i can use thing initialized in Base """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
 
-    @property 
+    @property
+    """ to getter """
     def width(self):
         return self.__width
+
     @width.setter
+    """to setter"""
     def width(self, value):
         self.__width = value
         if width isinstance != int:
             raise TypeError("width must be an integer")
         if width is <= 0:
             raise ValueError("width must > 0")
+
     @property
+    """ to getter """
     def height(self):
         return self.__height
+
     @height.setter
+    """ to  setter """
     def height(self, value):
         self.__height = value
         if height isinstance != int:
             raise TypeError("height must be an integer")
         if height is <= 0:
             raise ValueError("height must be > 0")
+
     @property
+    """ to getter """
     def x(self):
        return self.__x
+
     @x.setter
+    """ to setter """
     def x(self, value):
         self.__x = value
         if x isinstance != int:
@@ -49,10 +60,12 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
 
     @property
+    """ to getter """
     def y(self):
        return self.__y
 
     @y.setter
+    """ to setter """
     def y(self, value):
         self.__y = value
         if y isinstance != int:
