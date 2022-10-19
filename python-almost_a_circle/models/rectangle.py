2033c1d908d00a64f754f0b91b20f631bf352141
Blingblinggiirl <romibenitezz87@gmail.com>
2022-10-19 16:33:31 -0700

    update task 3 rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,4 +74,4 @@ class Rectangle(Base):
 
     def area(self):
         """ return the area value of the Rectangle instance """
-        return width * height
+        return self.__width * self.__height
