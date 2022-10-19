560c0314318e755b36c97054b57fc6e704dd66fa
Blingblinggiirl <romibenitezz87@gmail.com>
2022-10-19 08:11:40 -0700

    put the init first, add the returns in the getter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,42 +8,42 @@ from models.base import Base
 
 class Rectangle(Base):
     """ Rectangle """
-    @property 
-    def width(self):
-        self.__width
-    
-    @width.setter
-    def width(self, value):
-        self.__width = width
-
-    @property
-    def height(self):
-        self.__height
-    
-    @height.setter
-    def height(self, value):
-        self.__height = height
-
-    @property
-    def x(self):
-        self.__x
-
-    @x.setter
-    def x(self, value):
-        self.__x = x
-
-    @property
-    def y(self):
-        self.__y
-    
-    @y.setter
-    def y(self, value):
-        self.__y = y
-    
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """ init """
+    def __init__(self, width, height, x=0, y=0, id=None)
+       """ init """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property 
+    def width(self):
+       return self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+       return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+       return self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
