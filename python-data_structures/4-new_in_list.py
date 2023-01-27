891253f971d263a89d784d1e2f1457d5e1f32fd7
root <romibenitezz87@gmail.com>
2023-01-27 11:14:47 -0800

    change .append to .copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = []
-    new_list.append(my_list)
+    new_list = my_list.copy()
 
     if idx < 0:
         return new_list
     if idx >= len(my_list):
         return new_list
 
-    if my_list[idx] == element:
-        new_list[idx] = element
+    my_list[idx] == element
     return new_list
