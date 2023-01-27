c34f86df3f461c841e8dd3a50b22e1314176e6d4
root <romibenitezz87@gmail.com>
2023-01-27 14:33:23 -0800

    fix borde case

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,5 +2,6 @@
 def multiple_returns(sentence):
     if sentence is not None:
         return len(sentence), sentence[0]
-    else:
+    if sentence == "":
         return 0, None
+    return 0, None
