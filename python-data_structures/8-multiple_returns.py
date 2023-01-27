5bc6753adee5937920f1e00b9cea047ce3c902fe
root <romibenitezz87@gmail.com>
2023-01-27 14:49:11 -0800

    fix

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is not None:
-        return len(sentence), sentence[0]
+    if sentence is None:
+        return 0, None
     if sentence == "":
         return 0, None
-    return 0, None
+    if sentence is not None:
+        return len(sentence), sentence[0]
