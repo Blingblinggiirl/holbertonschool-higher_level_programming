5d1211e6f7635e5a98fb4ad2d6cd7b7e736faf97
root <romibenitezz87@gmail.com>
2023-01-27 15:30:01 -0800

    fix comparation

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    maxnum = None
+    maxnum = my_list[0]
     for idx in my_list:
         if maxnum is None or maxnum < idx:
             maxnum = idx
-        return(maxnum)
+    return maxnum
