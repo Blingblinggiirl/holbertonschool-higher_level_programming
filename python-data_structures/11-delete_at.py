98950a4d7add93df4363e42dd40ae2849509711d
root <romibenitezz87@gmail.com>
2023-01-28 08:43:36 -0800

    replace remove for del

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
+    new_list = []
+    if idx <= 0 or idx > len(my_list):
         return my_list
     else:
-       new_list = my_list.remove(idx)
-    return new_list
+        del my_list[idx]
+        new_list = my_list
+        return new_list
