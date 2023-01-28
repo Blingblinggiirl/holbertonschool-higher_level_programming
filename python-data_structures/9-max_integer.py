ef860cce8b64e36a858f20363e9e5ba31371104a
root <romibenitezz87@gmail.com>
2023-01-28 08:58:11 -0800

    exercise 9 and 12

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list is None or len(my_list) == 0:
         return None
     maxnum = my_list[0]
     for idx in my_list:
