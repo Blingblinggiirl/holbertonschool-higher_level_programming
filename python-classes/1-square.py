594f23877d1818234e985878db42e6d61f5d7c05
root <romibenitezz87@gmail.com>
2023-01-31 07:12:33 -0800

    size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Writing a Private instance attribute to class Square """
+
+
+class Square():
+    """ class that defines a square """
+
+    def __init__(self, size):
+        """ difine the size of the square """
+        self.size = size 
