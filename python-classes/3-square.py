65458c6333cd3de1646fb51c729ba2c54b7742b4
root <romibenitezz87@gmail.com>
2023-01-31 10:53:17 -0800

    tarea

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,15 +5,6 @@
 class Square():
     """ class that defines a square """
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """ difine the size of the square """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """ returns the area of a square using size """
-        return .__size * 2
-
