fab9bcc62ffb45fa35bcdd73d8cc47a134af1de4
root <romibenitezz87@gmail.com>
2023-01-31 09:25:57 -0800

    int

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Writing a Private instance attribute to class Squar """
+""" Writing a Private instance attribute to class Square """
 
 
 class Square():
@@ -7,10 +7,9 @@ class Square():
 
     def __init__(self, size=0):
         """ difine the size of the square """
-
         self.__size = size
-        
         if size not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("size must be >=0")
+
