ef3cb4e14e86f3a093a136cb262812b8d0f529d9
root <romibenitezz87@gmail.com>
2023-02-14 08:52:15 -0800

    1, width

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" making a Square """
+
+
+class Square:
+    """ define a square """
+
+    def __init__(self, width):
+        """ to init """
+
+        self._width = width
+
+    @property
+    def width(self):
+        """ to get width """
+        return self._width
+
+    @width.setter
+    def width(self, value):
+	""" to set """
+	
+	if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+
+
