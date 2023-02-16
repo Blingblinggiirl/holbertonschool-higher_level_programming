5d0d4fd98cd5c3d64a18e36d47a77c10001faab0
root <romibenitezz87@gmail.com>
2023-02-16 10:05:47 -0800

    commit

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+    """ This class will be the base 
+    of all other classes in this project. 
+    """
+
+    class Base:
+        """ Base class """
+        __nb_obkects = 0
+
+        def __init__(self, id=None):
+            """ class constructor """
+           if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id =  
+                
