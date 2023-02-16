def28e55859fd60f058ee71447803c57560a0656
root <romibenitezz87@gmail.com>
2023-02-16 10:10:30 -0800

    commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-""" This class will be the base 
-    of all other classes in this project. 
-    """
+""" is a base for other classes """
 
-    class Base:
-        """ Base class """
-        __nb_obkects = 0
 
-        def __init__(self, id=None):
-            """ class constructor """
-           if id is None:
+class Base:
+    """ class Base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ defines a private class attribute """
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
         else:
-            self.id =  
-                
+            self.id = id
+
